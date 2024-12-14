454c34faa3a4cb083ce87318da704c89c89cb61c
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-12-14 00:05:31 +0000

    add Hero component and integrate it into Home page layout

---
+++ portfolio/src/app/components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <section id="home" className="bg-gray-900 p-4">
+            <div className="flex justify-center mt-4">
+                <button className="text-gray-50 bg-slate-900 border border-white px-4 py-2 rounded-md">light!</button>
+            </div>
+            <h1 className="text-6xl text-center font-">I'm <span className="text-blue-600">Jenny</span>,<br />Product Designer</h1>
+        </section>
+    );
+}
